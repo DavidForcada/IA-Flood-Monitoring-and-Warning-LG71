98e67393e23bf0adff3951ff4edd4e11c506b96b
David Forcada <df431@cam.ac.uk>
2022-01-31 13:43:16 +0000

    does this work

--- Task1A.py
+++ Task1A.py
@@ -4,6 +4,7 @@
 
 from floodsystem.stationdata import build_station_list
 
+build_station_list()
 
 def run():
     """Requirements for Task 1A"""
@@ -25,3 +26,6 @@ def run():
 if __name__ == "__main__":
     print("*** Task 1A: CUED Part IA Flood Warning System ***")
     run()
+
+
+print("Hello there")
